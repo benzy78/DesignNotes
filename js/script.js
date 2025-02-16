2fb44f7e689300d7fd58adb5bf9975710d07d52c
benzy78 <westvillage815@gmail.com>
2025-02-16 17:14:35 +0900

    animation

--- js/script.js
+++ js/script.js
@@ -32,3 +32,15 @@ $(document).ready(function() {
       $(this).toggleClass("turn");
   });
 });
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  });
+});
